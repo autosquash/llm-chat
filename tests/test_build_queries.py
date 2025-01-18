0c9c7e542f2d9aab6248513f266cba2f1d44fd0f
autosquash <49131885+autosquash@users.noreply.github.com>
2025-01-18 16:34:53 +0100

    refactor test_build_queries.py

--- tests/test_build_queries.py
+++ tests/test_build_queries.py
@@ -1,4 +1,5 @@
 import unittest
+from dataclasses import dataclass
 from typing import Mapping, TypeGuard
 
 from src.models.placeholders import (
@@ -21,36 +22,52 @@ def is_substitutions_dict(d: Mapping[str, str]) -> TypeGuard[Substitutions]:
     )
 
 
+@dataclass
+class Case:
+    description: str
+    substitutions: Substitutions
+    raw_query: str
+    expected: list[str]
+
+
+cases = [
+    Case(
+        description="No placeholders",
+        substitutions=as_substitutions({}),
+        raw_query="hello",
+        expected=["hello"],
+    ),
+    Case(
+        description="With placeholders",
+        substitutions=as_substitutions({"{country}": "France"}),
+        raw_query="What is the capital of {country}?",
+        expected=["What is the capital of France?"],
+    ),
+    Case(
+        description="Using the for command with one element",
+        substitutions=as_substitutions({"{start}": "/for 1", "{end}": "5"}),
+        raw_query="List numbers from {start} to {end}",
+        expected=["List numbers from 1 to 5"],
+    ),
+    Case(
+        description="Using the for command with two elements",
+        substitutions=as_substitutions({"{start}": "/for 10,20", "{end}": "50"}),
+        raw_query="List numbers from {start} to {end}",
+        expected=["List numbers from 10 to 50", "List numbers from 20 to 50"],
+    ),
+]
+
+
 class TestBuildqueries(unittest.TestCase):
     """The format of the placeholders is irrelevant here"""
 
-    def test_build_queries_no_placeholders(self) -> None:
-        substitutions = as_substitutions({})
-        expected = ["hello"]
-        self.assertEqual(
-            build_queries("hello", substitutions),
-            expected,
-        )
-
-    def test_build_queries_with_placeholders(self) -> None:
-        raw_query = "What is the capital of {country}?"
-        substitutions = as_substitutions({"{country}": "France"})
-        expected = ["What is the capital of France?"]
-        self.assertEqual(build_queries(raw_query, substitutions), expected)
-
-    def test_build_queries_with_for_command_with_one_element(self) -> None:
-        substitutions = as_substitutions({"{start}": "/for 1", "{end}": "5"})
-        expected = ["List numbers from 1 to 5"]
-        self.assertEqual(
-            build_queries("List numbers from {start} to {end}", substitutions), expected
-        )
-
-    def test_build_queries_with_for_command_with_two_elements(self) -> None:
-        substitutions = as_substitutions({"{start}": "/for 10,20", "{end}": "50"})
-        expected = ["List numbers from 10 to 50", "List numbers from 20 to 50"]
-        self.assertEqual(
-            build_queries("List numbers from {start} to {end}", substitutions), expected
-        )
+    def test_build_queries(self) -> None:
+        for case in cases:
+            self.assertEqual(
+                build_queries(case.raw_query, case.substitutions),
+                case.expected,
+                case.description,
+            )
 
     def test_build_queries_multiple_for_not_supported(self) -> None:
         substitutions = as_substitutions({"{start}": "/for 1", "{end}": "/for 5"})
