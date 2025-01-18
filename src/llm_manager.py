1ce75a90652106a5dc27274474c5e06ff0661968
autosquash <49131885+autosquash@users.noreply.github.com>
2025-01-18 11:21:50 +0100

    refactor

--- src/llm_manager.py
+++ src/llm_manager.py
@@ -1,10 +1,12 @@
-from dataclasses import dataclass
-
 from src.model_manager import ModelManager
 from src.protocols import ChatRepositoryProtocol
 
 
-@dataclass
 class LLM_Manager:
+    __slots__ = ("repository", "model_manager")
     repository: ChatRepositoryProtocol
     model_manager: ModelManager
+
+    def __init__(self, repository: ChatRepositoryProtocol, model_manager: ModelManager):
+        self.repository = repository
+        self.model_manager = model_manager
